b387390a5c9771336133cdefd08bab1729d5f32a
Emil Blennow <emil.blennow@sony.com>
2023-09-07 15:55:02 +0200

    complete on tab

--- lua/blennster/completions.lua
+++ lua/blennster/completions.lua
@@ -53,8 +53,7 @@ M.lazy = {
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete {},
-
+          -- ['<C-Space>'] = cmp.mapping.complete {},
           ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
@@ -64,6 +63,8 @@ M.lazy = {
               cmp.select_next_item()
             elseif luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
+            elseif not cmp.visible() then
+              cmp.complete()
             else
               fallback()
             end
