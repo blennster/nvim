327c5dc0c23f269a83a34db60c7888870e294849
Emil Blennow <emil.blennow@sony.com>
2023-09-28 16:54:54 +0200

    update lualine

--- lua/blennster/ui.lua
+++ lua/blennster/ui.lua
@@ -7,6 +7,15 @@ M.lazy = {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+      routes = {
+        { -- filter write messages "xxxL, xxxB"
+          filter = {
+            event = "msg_show",
+            find = "%dL",
+          },
+          opts = { skip = true },
+        },
+      },
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
@@ -32,18 +41,21 @@ M.lazy = {
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
-    dependencies = "SmiteshP/nvim-navic",
+    dependencies = {
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons'
+    },
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
+        lualine_a = { "branch" },
+        lualine_b = { "filetype" },
         lualine_c = {
           {
             "diagnostics",
@@ -54,38 +66,16 @@ M.lazy = {
               hint = icons.diagnostics.Hint,
             },
           },
-          {
-            "filetype",
-            icon_only = true,
-            separator = "",
-            padding = { left = 1, right = 0 }
-          },
-          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          { "filename", path = 1, symbols = { modified = icons.git.modified, readonly = "", unnamed = "" } },
           -- stylua: ignore
           {
             function() return require("nvim-navic").get_location() end,
             cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
           },
         },
-        lualine_x = {
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-          },
-        },
-        lualine_y = {
-          { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
+        lualine_x = { "fileformat" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
       extensions = { "neo-tree", "lazy" },
     },
