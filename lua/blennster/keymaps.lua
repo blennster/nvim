3105dee35585fc47e89bdf81c865a6300c2c3f2d
Emil Blennow <emil.blennow99@gmail.com>
2024-10-10 08:46:17 +0200

    fix term keymaps

--- lua/blennster/keymaps.lua
+++ lua/blennster/keymaps.lua
@@ -315,12 +315,11 @@ end, { desc = 'Toggle Chat' })
 -- Fix terminal
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  map('t', '<esc>', [[<C-\><C-n>]], opts)
-  map('t', 'jk', [[<C-\><C-n>]], opts)
-  map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  map('t', '<C-esc>', [[<C-\><C-n>]], opts)
+  -- map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  -- map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  -- map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  -- map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
   map('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
