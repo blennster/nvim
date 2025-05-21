53aef11a4e0e35212740c3b25fa5b0732675e550
Emil Blennow <emil.blennow99@gmail.com>
2025-05-21 11:50:38 +0200

    fix deprecated get_install_path

--- lua/blennster/servers.lua
+++ lua/blennster/servers.lua
@@ -52,8 +52,7 @@ M.servers = function ()
   })
 
   local mason_registry = require('mason-registry')
-  local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() ..
-      '/node_modules/@vue/language-server'
+  local vue_language_server_path = vim.fn.expand '$MASON/packages/vue-language-server/node_modules/@vue/language-server'
 
   -- Enable the following language servers
   local servers = {
